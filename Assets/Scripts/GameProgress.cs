eb31bf75d09f2ebaf33679c13da21571a5bf93c9
d34thwings <d34thwings@gmail.com>
2020-02-01 17:07:11 +0000

    Rising water level

--- Assets/Scripts/GameProgress.cs
+++ Assets/Scripts/GameProgress.cs
@@ -7,11 +7,18 @@ public class GameProgress : MonoBehaviour
     public float BoatSpeed = 1.0f;
     public float GameDuration = 60 * 3;
     public float TimeBetweenEvents = 7;
+    public float RockHitDelay = 5;
+    public float WaterLevelIncreasePerHole = 0.2f;
+    public float MaxWaterLevel = 9;
+    public Transform BoatWrapper;
 
     public float Progress { get; private set; }
     
     private float endTime;
     private float lastEventTime;
+    private float nextRockHit = -1;
+    private int holesInHull = 0;
+    private float waterLevel = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +31,27 @@ public class GameProgress : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (nextRockHit != -1 && nextRockHit < Time.time)
+        {
+            Debug.Log("Hit by rock!");
+            nextRockHit = -1;
+            holesInHull += 1;
+        }
+
+        var targetWaterLvel = new Vector3(BoatWrapper.position.x, -waterLevel, BoatWrapper.position.z);
+        BoatWrapper.position = Vector3.Lerp(BoatWrapper.position, targetWaterLvel, Time.deltaTime);
     }
 
     void TriggerEvent()
     {
         Debug.Log("Rock approaching!");
+        nextRockHit = Time.time + RockHitDelay;
+        lastEventTime = Time.time;
+    }
+
+    public void DodgeRock()
+    {
+        nextRockHit = -1;
     }
 
     void Lose()
@@ -44,11 +66,13 @@ public class GameProgress : MonoBehaviour
 
     IEnumerator OnTimerTick()
     {
-        while (endTime > Time.time && Progress < 100)
+        while (endTime > Time.time && Progress < 100 && waterLevel < MaxWaterLevel)
         {
             yield return new WaitForSeconds(1);
             Progress = Mathf.Min(Progress + BoatSpeed, 100);
-            Debug.Log("Time left: " + (endTime - Time.time) / 1000 + "s; Progress: " + Progress + "%");
+            Debug.Log("Time left: " + Mathf.RoundToInt(endTime - Time.time) + "s; Progress: " + Progress + "%; Water level: " + waterLevel);
+
+            waterLevel += holesInHull * WaterLevelIncreasePerHole;
 
             if (lastEventTime + TimeBetweenEvents < Time.time)
             {
