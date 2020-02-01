b9b670f434df4372d175196ca8fe59da1532225a
d34thwings <d34thwings@gmail.com>
2020-02-01 00:15:51 +0000

    Add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 movement;
+    public float Speed = 0.5f;
+
+    private PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        movement = inputValue.Get<Vector2>();
+    }
+
+    public void MovePlayer()
+    {
+        transform.Translate(new Vector3(movement.x, 0, movement.y) * Speed);
+    }
+}
